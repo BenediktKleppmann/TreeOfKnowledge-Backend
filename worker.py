1a1622ebba57d532cd6754e74b5bd908c2a23f9b
Benedikt Kleppmann <benedikt@kleppmann.de>
2018-11-28 19:52:40 +0000

    new try with some changes to the procfile - following the instructions from https://devcenter.heroku.com/articles/celery-heroku

--- worker.py
+++ worker.py
@@ -8,6 +8,7 @@ import os
 # $ celery -A worker worker --loglevel=debug
 redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
 app = Celery(__name__, backend='rpc://', broker=redis_url)
+app.conf.update(BROKER_URL=redis_url, CELERY_RESULT_BACKEND=redis_url)
 
 ## easier, if you don't care about exceptions:
 # integrate = app.task(approx)
