daced397bc84cec63e59bf5dc389cb265590fe88
Benedikt Kleppmann <benedikt@kleppmann.de>
2018-11-29 19:30:39 +0000

    even more tiny change - let's hope heroku can withstand that

--- app.py
+++ app.py
@@ -66,7 +66,9 @@ def put_task():
 # ===============================================================
 # ===============================================================
 
-
+@app.route('test/', methods=['PUT'])
+def test():
+    return 'test'
 
 # @app.route('/status/<task_id>')
 # def taskstatus(task_id):
