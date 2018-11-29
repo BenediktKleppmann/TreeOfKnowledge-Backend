9e7646f547f6bb8881cf74c12bd90c3e278e839b
Benedikt Kleppmann <benedikt@kleppmann.de>
2018-11-29 18:44:43 +0000

    slowly changing the flask-app to a version with progress-updates of the celery tasks, but no too sudden changes - want to keep it working

--- app.py
+++ app.py
@@ -61,6 +61,52 @@ def put_task():
     response = {'result': task_id}
     return jsonify(response)
 
+
+# ===============================================================
+# ===============================================================
+# ===============================================================
+
+
+
+# @app.route('/status/<task_id>')
+# def taskstatus(task_id):
+#     task = long_task.AsyncResult(task_id)
+#     if task.state == 'PENDING':
+#         response = {
+#             'state': task.state,
+#             'current': 0,
+#             'total': 1,
+#             'status': 'Pending...'
+#         }
+#     elif task.state != 'FAILURE':
+#         response = {
+#             'state': task.state,
+#             'current': task.info.get('current', 0),
+#             'total': task.info.get('total', 1),
+#             'status': task.info.get('status', '')
+#         }
+#         if 'result' in task.info:
+#             response['result'] = task.info['result']
+#     else:
+#         # something went wrong in the background job
+#         response = {
+#             'state': task.state,
+#             'current': 1,
+#             'total': 1,
+#             'status': str(task.info),  # this is the exception raised
+#         }
+#     return jsonify(response)
+
+
+
+# @app.route('start_simulation/', methods=['GET'])
+# def start_simulation(request):
+#     task = run_simulations.apply_async()
+#     return jsonify({}), 202, {'Location': url_for('taskstatus',
+#                                                   task_id=task.id)}
+
+
+
+
 if __name__ == '__main__':
     app.run(debug=True)
-    
